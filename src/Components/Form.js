724909c5b868dcd25d03d588db8010db6734c810
daniel <dani.ajala@yahoo.com>
2023-01-09 22:16:18 -0300

    jogo

---
+++ src/Components/Form.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Form.css"
+
+
+const Form =()=>{
+    return(
+        <div>
+            <form>
+                <label>nome</label>
+                <input type={"text"} name="nome"></input>
+            </form>
+        </div>
+    )
+}
+
+export default Form
